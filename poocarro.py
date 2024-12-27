0a00fddb0f414db974fd3f1401c09d61116f8b56
@dioelitonpassos <dioelitonpassos@hotmail.com>
2024-12-27 18:34:37 -0300

    trabalhando Herança

---
+++ poocarro.py
@@ -0,0 +1,46 @@
+class Carro:
+    # Definir o construtor
+    def __init__(self, marca, ano, potencia, cor):
+        self.marca = marca
+        self.ano = ano
+        self.potencia = potencia
+        self.cor = cor
+
+class Moto(Carro):
+    # Redefinir o construtor se precisar de atributos adicionais específicos para Moto
+    def __init__(self, marca, ano, potencia, cor):
+        super().__init__(marca, ano, potencia, cor)
+
+    def ligar(self):
+        return f"A moto {self.marca} está agora ligada."
+
+    def desligar(self):
+        return f"A moto {self.cor} do ano {self.ano} está agora desligada."
+
+    def acelerar(self):
+        return f"A moto {self.marca} está acelerando com potência de {self.potencia}."
+
+    def status(self):
+        return f"Uma moto da marca {self.marca}, do ano {self.ano}, com potência de {self.potencia}, sendo bem {self.cor}."
+
+# Criando instâncias de Carro e Moto
+carro01 = Carro('Toyota', 2022, '150 CV', 'azul')
+moto1 = Moto('Honda', 2016, '2.0', 'vermelha')
+
+# Chamando métodos da classe e imprimindo os resultados
+print(moto1.ligar())
+print(moto1.acelerar())
+print(moto1.desligar())
+print(moto1.status())
+
+
+
+
+
+
+
+
+
+
+
+        
