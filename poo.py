9de3c71663386229836f182dc78e22da6a0d8d13
@dioelitonpassos <dioelitonpassos@hotmail.com>
2024-12-27 22:02:09 -0300

    herança

--- poo.py
+++ poo.py
@@ -10,14 +10,21 @@ class Pessoa:
 
     def perguntar(self):
         return f"Tudo bem, meu nome é {self.nome} e tenho {self.idade} anos e moro na rua {self.rua} em recife PE"
-    
-    
 
-    # Criando uma instância de Pessoa
+
+class Cadastro (Pessoa):
+    def __init__(self, idade, nome, rua, cpf):
+        super().__init__(idade, nome, rua, cpf)
+
 pessoa1 = Pessoa(30,"joão","rua sem nome","123.456")
 pessoa2 = Pessoa(20,"Dioeliton","Rua Bartolomeu Marques","04041050413")
+cadastro1 = Cadastro(12,"eduardo","rua Ubiratan","040")
 
+    
+    
+
+ 
 # Chamando um método da classe
 print(pessoa1.perguntar())
 print(pessoa2.perguntar())
-# Saída: Olá, meu nome é João e tenho 30 anosp
+print(cadastro1.perguntar())
