172d202583132993a8ddf25d409cebe60dc8d774
@dioelitonpassos <dioelitonpassos@hotmail.com>
2024-12-30 16:57:43 -0300

    importando classe carro para o main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from poobanco import ContaBanco
 from poobanco import Cadastro 
+from poocarro import Carro
+
 
 conta = ContaBanco("João Silva", "123.456.789-00", "Rua Sem Nome, 123", 30)
 cadastro1= Cadastro("Pedro", "123.456.789", "Rua  123", 40)
+carro1 = Carro("voiage", "123", "Rua ubirajara", 1940)
 
 # Chamando métodos da classe
 print(conta.depositar(1500))
@@ -12,5 +15,6 @@ print(conta.ver_saldo())
 print(conta.ver_extrato())
 print(conta)
 print(cadastro1.cadastrar())
+print(carro1.ligar())
 
 
